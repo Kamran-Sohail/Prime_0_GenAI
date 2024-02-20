be485e4f0989ec405fe5c08041f8298164a28668
Kamran-Sohail <106171774+Kamran-Sohail@users.noreply.github.com>
2024-02-21 03:08:23 +0500

    Update prime_0_GenAI.cs

--- prime_0_GenAI.cs
+++ prime_0_GenAI.cs
@@ -5,18 +5,17 @@ static void Main(string[] args)
     for (int I = 2; I < 100; I++)
     {
         bool isPrime = true;
-        if (Is_Primer_Number(I) == true)
-            Console.WriteLine(I);
+        for (int j = 2; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.WriteLine(i);
+                }
     }
-
-    Console.ReadKey();
-}
-
-static bool Is_Primer_Number(int Number)
-{
-    for (int I = 2; I < Number; I++)
-    {
-        if ((Number % I) == 0) return false;
-    }
-    return true;
 }
